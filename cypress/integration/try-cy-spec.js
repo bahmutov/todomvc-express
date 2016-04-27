3a3c4425c4eab7c3351ebca171af8af538cd6144
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-27 14:26:44 -0400

    chore(test): tests working in major browsers

--- cypress/integration/try-cy-spec.js
+++ cypress/integration/try-cy-spec.js
@@ -3,18 +3,35 @@
 // const la = require('lazy-ass')
 // const is = require('check-more-types')
 
+// using function declaration instead of arrow functions
+// so Safari browser understands
+
 describe('a cy unit test', function () {
   it('is ok', function () {
     expect(true).to.be.true
   })
 
-  it('understands arrow functions', () => {
+  it('understands arrow functions', function () {
     expect(false).to.be.false
   })
 })
 
-describe('todomvc app', () => {
-  it('can load the app', () => {
+describe('todomvc app', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:3000')
+  })
+
+  it('can load the app', function () {
+    cy.get('h1').should('contain', 'todos')
+  })
+
+  it('can insert new todo', function () {
+    cy.get('.new-todo')
+      .type('test cy{enter}')
+      .get('ul.todo-list')
+        .find('li').should('not.be.empty')
+        .find('.toggle').check({force: true})
+      .get('ul.filters').contains('Active').click()
+      .hash().should('eq', '#/active')
   })
 })
