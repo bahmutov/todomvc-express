c5f0f63d203dc4d383dbdebdf1e84e4827fccdf4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-18 09:42:51 -0500

    comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,13 +9,22 @@ beforeEach(function () {
 
 it('deletes all items', () => {
   cy.get('.todo-list li')
+    // there might be no todos to delete
+    // so disable the built-in existence assertion
+    // inside the cy.get query command
     .should(Cypress._.noop)
     .then(($list) => {
       if (!$list.length) {
         cy.log('No todos, nothing to delete')
       } else {
+        // click on each item's destroy button
         $list.each((k, $el) => {
-          cy.wrap($el).find('.destroy').click({ force: true })
+          cy.wrap($el)
+            .find('.destroy')
+            // the element becomes visible only on hover
+            // so have to force it to click without
+            // checking visibility first
+            .click({ force: true })
         })
       }
     })
